df9b51bcc462e9cccc61664f1dddfd8549031e17
Vineeth Chitteti <vineethreddy.agusta@gmail.com>
2019-09-23 18:54:28 -0700

    US23: Computation logic for evaluating expressions

--- src/Grade912.js
+++ src/Grade912.js
@@ -1,8 +1,75 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react';
+import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from "./components/KeyPadComponent";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Grade912 extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: ""
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+        else if(button === "CE"){
+            this.backspace()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        try {
+            this.setState({
+                // eslint-disable-next-line
+                result: (eval(this.state.result) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+    backspace = () => {
+        this.setState({
+            result: this.state.result.slice(0, -1)
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="calculator-body">
+                    <h1>Simple Calculator</h1>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Grade912;
