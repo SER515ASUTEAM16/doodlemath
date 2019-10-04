fd277160bbe07acfff8fbe14b6cadfd863d2f71d
Vineeth Chitteti <vineethreddy.agusta@gmail.com>
2019-10-03 23:12:02 -0700

    US9: Adding functionality for trignometry buttons

--- src/Grade912.js
+++ src/Grade912.js
@@ -16,19 +16,68 @@ class Grade912 extends Component {
 
         if(button === "="){
             this.calculate()
-        }
-
-        else if(button === "C"){
+        } else if(button === "C"){
             this.reset()
-        }
-        else if(button === "CE"){
+        } else if(button === "CE"){
             this.backspace()
-        }
+        } else if(button === "sin"){
 
-        else {
+        } else {
             this.setState({
                 result: this.state.result + button
             })
+        }
+
+        switch(button) {
+            case "=":
+                this.calculate()
+                break;
+            case "C":
+                this.reset()
+                break;
+            case "CE":
+                this.backspace()
+                break;
+            case "sin":
+                this.setState({
+                    // eslint-disable-next-line
+                    result: (Math.sin(this.state.result) || "" ) + ""
+                })
+                break;
+            case "cos":
+                this.setState({
+                    // eslint-disable-next-line
+                    result: (Math.cos(this.state.result) || "" ) + ""
+                })
+                break;
+            case "tan":
+                this.setState({
+                    // eslint-disable-next-line
+                    result: (Math.tan(this.state.result) || "" ) + ""
+                })
+                break;
+            case "sec":
+                this.setState({
+                    // eslint-disable-next-line
+                    result: ((1 / Math.cos(this.state.result)) || "" ) + ""
+                })
+                break;
+            case "cosec":
+                this.setState({
+                    // eslint-disable-next-line
+                    result: ((1 / Math.sin(this.state.result)) || "" ) + ""
+                })
+                break;
+            case "cot":
+                this.setState({
+                    // eslint-disable-next-line
+                    result: ((1 / Math.cot(this.state.result)) || "" ) + ""
+                })
+                break;
+            default:
+                this.setState({
+                    result: this.state.result + button
+                })
         }
     };
 
