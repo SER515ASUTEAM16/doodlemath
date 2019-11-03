a4ed03c34bc040db9b8e8e20085dc60382de6485
GuruPreetam <54914284+GuruPreetam@users.noreply.github.com>
2019-11-03 16:39:03 -0700

    split exponential into indiviual units of intermediate, added exp

--- src/blocks/HigherGrade.js
+++ src/blocks/HigherGrade.js
@@ -61,7 +61,18 @@ const toolbox = `
             </Category>
             
             <category name="Exponential">
-                <block type="math_single">
+                <block type="math_single" editable="false">
+                    <field name="OP">EXP</field>
+                    <value name="A">
+                        <shadow type="math_number">
+                            <field name="NUM">1</field>
+                        </shadow>
+                    </value>
+                    <value name="B">
+                        <shadow type="math_number">
+                            <field name="NUM">1</field>
+                        </shadow>
+                    </value>
                 </block>
             </category>
          </xml>`
