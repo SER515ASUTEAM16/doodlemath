13f27362000a32e4fc38c9093f7b8f37f6f634d9
Akash Devdhar <neospeed83@gmail.com>
2019-11-20 15:40:54 -0700

    US#68 Task#81 updated App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@
  */
 
 //FRONT END blocks
-
-
 import React from 'react';
 import './App.css';
 import LowerGrade from "./blocks/LowerGrade";
@@ -28,170 +26,165 @@ import Teacher from "./Teacher";
 
 function UserLogin(props) {
 
-
     if (props.role === "teacher") {
-        return (
-            <div>
-                <Teacher/>
-            </div>)
+        return (<Teacher/>)
+    }
+    else if (props.role === "student") {
+        if (props.grade === "1-5") {
+            return (<LowerGrade/>)
+        }
+        else if (props.grade === "6-8") {
+            return (<IntermediateGrade/>)
+        }
+        if (props.grade === "9-12") {
+            return (<AdvancedGrade/>)
+        } else {
+            return(<h1>Invalid grade</h1>)
+        }
     }
     else {
-        return (
-            <div>
-                <Router>
-                    <Route path="/grade1-5" component={LowerGrade}/>
-                    <Route path="/grade5-8" component={IntermediateGrade}/>
-                    <Route path="/grade9-12" component={AdvancedGrade}/>
-                    <Link to="/grade1-5">
-                        <button>Beginner</button>
-                    </Link>
-                    <Link to="/grade5-8">
-                        <button>Intermediate</button>
-                    </Link>
-                    <Link to="/grade9-12">
-                        <button>Advanced</button>
-                    </Link>
-                </Router>
-            </div>
-        )
+        return(<h1>Invalid role</h1>)
     }
 }
+
 
 //LOGIN PAGE UI
 
-function GustLogin(props) {
-    return (
-        <MuiThemeProvider>
-            <div>
-                <h2>Sign in</h2>
-                <TextField
-                    value={props.username}
-                    onChange={props.onUserChange}
-                    hintText="Enter your Username"
-                    floatingLabelText="Username"
-                />
-                <br/>
-                <TextField
-                    value={props.password}
-                    onChange={props.onPassChange}
-                    type="password"
-                    hintText="Enter your Password"
-                    floatingLabelText="Password"
-                />
-                <br/>
-                <RaisedButton label="Submit" primary={true} onClick={props.onClick}/>
-            </div>
-        </MuiThemeProvider>
-    );
-}
+    function GustLogin(props) {
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <h2>Sign in</h2>
+                    <TextField
+                        value={props.username}
+                        onChange={props.onUserChange}
+                        hintText="Enter your Username"
+                        floatingLabelText="Username"
+                    />
+                    <br/>
+                    <TextField
+                        value={props.password}
+                        onChange={props.onPassChange}
+                        type="password"
+                        hintText="Enter your Password"
+                        floatingLabelText="Password"
+                    />
+                    <br/>
+                    <RaisedButton label="Submit" primary={true} onClick={props.onClick}/>
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 
 //CONTROL LOGIN EVENTS
-class LoginControl extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.handleUserNameInput = this.handleUserNameInput.bind(this);
-        this.handlePassInput = this.handlePassInput.bind(this);
+    class LoginControl extends React.Component {
+        constructor(props) {
+            super(props);
+            this.handleLoginClick = this.handleLoginClick.bind(this);
+            this.handleLogoutClick = this.handleLogoutClick.bind(this);
+            this.handleUserNameInput = this.handleUserNameInput.bind(this);
+            this.handlePassInput = this.handlePassInput.bind(this);
 
-        this.state = {
-            isLoggedIn: false,
-            userName: '',
-            password: '',
-            userRole: ''
-        };
-    }
-
-    //When LOGIN is pressed
-    handleLoginClick(e) {
-        const isEmail = this.state.userName.includes("@");
-        if (isEmail) {
-            let input = {
-                email: this.state.userName,
-                password: this.state.password
+            this.state = {
+                isLoggedIn: false,
+                userName: '',
+                password: '',
+                userRole: '',
+                grade:''
             };
+        }
 
-            fetch('http://localhost:8080/auth/loginByEmail', {
-                method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(input)
-            }).then(r => r.json())
-                .then((data) => {
-                    console.log(data);
-                    if (data.email === this.state.userName) {
-                        this.setState({
-                            isLoggedIn: true,
-                            userRole: data.role
-                        });
-                        console.log("Login success");
-                    }
-                })
-        } else {
-            let input = {
-                name: this.state.userName,
-                password: this.state.password
-            };
+        //When LOGIN is pressed
+        handleLoginClick(e) {
+            const isEmail = this.state.userName.includes("@");
+            if (isEmail) {
+                let input = {
+                    email: this.state.userName,
+                    password: this.state.password
+                };
 
-            fetch('http://localhost:8080/auth/loginByName', {
-                method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify(input)
-            }).then(r => r.json())
-                .then((data) => {
-                    console.log(data);
-                    if (data.name === this.state.userName) {
-                        this.setState({
-                            isLoggedIn: true,
-                            userRole: data.role
-                        });
-                        console.log("Login success");
-                    }
-                })
+                fetch('http://localhost:8080/auth/loginByEmail', {
+                    method: 'post',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(input)
+                }).then(r => r.json())
+                    .then((data) => {
+                        console.log(data);
+                        if (data.email === this.state.userName) {
+                            this.setState({
+                                isLoggedIn: true,
+                                userRole: data.role,
+                                grade:data.grade
+                            });
+                            console.log("Login success");
+                        }
+                    })
+            } else {
+                let input = {
+                    name: this.state.userName,
+                    password: this.state.password
+                };
+
+                fetch('http://localhost:8080/auth/loginByName', {
+                    method: 'post',
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(input)
+                }).then(r => r.json())
+                    .then((data) => {
+                        console.log(data);
+                        if (data.name === this.state.userName) {
+                            this.setState({
+                                isLoggedIn: true,
+                                userRole: data.role,
+                                grade:data.grade
+                            });
+                            console.log("Login success");
+                        }
+                    })
+            }
+        }
+
+        //WHEN LOGOUT is pressed
+        handleLogoutClick(e) {
+            this.setState({isLoggedIn: false});
+        }
+
+        //When username field changed
+        handleUserNameInput(e) {
+            this.setState({userName: e.target.value});
+        }
+
+        //When password field changed
+        handlePassInput(e) {
+            this.setState({password: e.target.value});
+        }
+
+        render() {                                          //prints LOGIN UI
+            if (this.state.isLoggedIn === false) {
+                return (
+                    <div className="App">
+                        <header className="App-header">
+                            <p>Welcome to DoodleMath!</p>
+                        </header>
+                        <GustLogin
+                            username={this.state.userName}
+                            password={this.state.password}
+                            onUserChange={this.handleUserNameInput}
+                            onPassChange={this.handlePassInput}
+                            onClick={this.handleLoginClick}
+                        />
+                    </div>
+                );
+            } else {
+                return (
+                    <div className="App">
+                        <header className="App-header">
+                            <p>Welcome to DoodleMath!</p>
+                        </header>
+                        <UserLogin grade={this.state.grade} role={this.state.userRole}/>
+                    </div>
+                );
+            }
         }
     }
-
-    //WHEN LOGOUT is pressed
-    handleLogoutClick(e) {
-        this.setState({isLoggedIn: false});
-    }
-
-    //When username field changed
-    handleUserNameInput(e) {
-        this.setState({userName: e.target.value});
-    }
-
-    //When password field changed
-    handlePassInput(e) {
-        this.setState({password: e.target.value});
-    }
-
-    render() {                                          //prints LOGIN UI
-        if (this.state.isLoggedIn === false) {
-            return (
-                <div className="App">
-                    <header className="App-header">
-                        <p>Welcome to DoodleMath!</p>
-                    </header>
-                    <GustLogin
-                        username={this.state.userName}
-                        password={this.state.password}
-                        onUserChange={this.handleUserNameInput}
-                        onPassChange={this.handlePassInput}
-                        onClick={this.handleLoginClick}
-                    />
-                </div>
-            );
-        } else {
-            return (
-                <div className="App">
-                    <header className="App-header">
-                        <p>Welcome to DoodleMath!</p>
-                    </header>
-                    <UserLogin role={this.state.userRole}/>
-                </div>
-            );
-        }
-    }
-}
-
-export default LoginControl;
+    export default LoginControl;
