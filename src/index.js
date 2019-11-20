d31539aa800d71c76fe54b77bf983241215db9e2
Akash Devdhar <neospeed83@gmail.com>
2019-11-20 15:34:32 -0700

    US#68 task#81 Implemented Role and Grade based Routing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import LoginControl from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<LoginControl />, document.getElementById('root'));
+
+ReactDOM.render(<LoginControl/>, document.getElementById('root'));
 
 // If you want your app to work  offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
